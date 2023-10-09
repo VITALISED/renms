37e6b33e289f7e641f553e0836cfe862128bd04d
Will <39478251+VITALISED@users.noreply.github.com>
2023-10-09 23:15:10 +1100

    Work on heridium

--- metadata/heridium/analysis.cpp
+++ metadata/heridium/analysis.cpp
@@ -9,3 +9,14 @@ void Heridium_Analysis_DoLookupIter()
         //handle crap
     }
 }
+
+uintptr_t Heridium_Analysis_UnpackClassPointerWriteFunc(LPVOID ClassPointerWriteFunction)
+{
+    //
+    uintptr_t cast_addr = (uintptr_t)ClassPointerWriteFunction + 0xBAD;
+
+    //somehow resolve and jump to cast
+
+    // return address of what's in the mov call in Cast()
+    return cast_addr + 0xC;
+}
--- metadata/heridium/analysis.h
+++ metadata/heridium/analysis.h
@@ -7,15 +7,5 @@
 using namespace NMS;
 
 void Heridium_Analysis_DoLookupIter();
-
 //HACK: since I lack the braincells to figure out how hashes are generated we will instead retrieve them from the generated functions.
-constexpr uintptr_t Heridium_Analysis_UnpackClassPointerWriteFunc(LPVOID ClassPointerWriteFunction)
-{
-    //
-    uintptr_t cast_addr = (uintptr_t)ClassPointerWriteFunction + 0xBAD;
-
-    //somehow resolve and jump to cast
-
-    // return address of what's in the mov call in Cast()
-    return cast_addr + 0xC;
-}
+uintptr_t Heridium_Analysis_UnpackClassPointerWriteFunc(LPVOID ClassPointerWriteFunction);
