7f7b31e25dc48755af5693ccc3ac4e682347a453
Gavin Mitchell <gavator2345@outlook.com>
2023-10-21 23:06:36 -0500

    Removed redundant code

--- metadata/heridium/launcher/main.cpp
+++ metadata/heridium/launcher/main.cpp
@@ -8,11 +8,7 @@
 #endif
 
 void WaitToClose(int code) {
-    if (code == -1) {   //started without elevation
-        exit(code);
-
-    } else if (code == 0) {
-        std::cout << "\n";
+    if (code == 0) {
         for (int i = 5; i >= 0; i--) {
             std::cout << std::format("\rClosing in {}...", i) << std::flush;
             Sleep(1000);
@@ -71,10 +67,6 @@ int main(int argc, char** argv) {
         auto nmsProcess = CreateProcessFrozen(argv[1]);
         std::cout << "Injecting the DLL...\n" << std::flush;
         InjectDLL(heridiumPath, nmsProcess.hProcess);
-
-        //Todo: Not this. Whenever we got GcApplication hooked, wait until that's actually initialized before unfreezing NMS.
-        Sleep(5000);
-        ResumeThread(nmsProcess.hThread);
 
         std::cout << "Injection successful!\n" << std::endl;
         WaitToClose(0);
