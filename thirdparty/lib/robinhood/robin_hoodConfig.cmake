47a9ae2822fe15cf8ad0e05e1f1c926b21ac8d43
Gavin Mitchell <gavator234@gmail.com>
2023-10-23 19:41:19 -0500

    Actually removed it this time

--- thirdparty/lib/robinhood/robin_hoodConfig.cmake
+++
@@ -1,1 +0,0 @@
-include("${CMAKE_CURRENT_LIST_DIR}/robin_hood-targets.cmake")
--- thirdparty/lib/spdlog/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/gavat/OneDrive/Desktop/codingstuff/renms/build/_deps/lib_spdlog-src/tests
-# Build directory: C:/Users/gavat/OneDrive/Desktop/codingstuff/renms/thirdparty/lib/spdlog/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(spdlog-utests "C:/Users/gavat/OneDrive/Desktop/codingstuff/renms/bin/spdlog-utests.exe")
-set_tests_properties(spdlog-utests PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/gavat/OneDrive/Desktop/codingstuff/renms/build/_deps/lib_spdlog-src/tests/CMakeLists.txt;23;add_test;C:/Users/gavat/OneDrive/Desktop/codingstuff/renms/build/_deps/lib_spdlog-src/tests/CMakeLists.txt;0;")
