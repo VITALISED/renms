e9aba6add1c8461431be95ea72e87f80685d1c43
Will <39478251+VITALISED@users.noreply.github.com>
2023-11-09 15:51:42 +1100

    Add PF headers

---
+++ renms/iat/playfab.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <renms.h>
+
+RENMS_BEGIN
+
+void CreatePlayFabHooks();
+
+RENMS_END
---
+++ renms/include/playfab_osx/PlayFabMultiplayer.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+//! Project version number for PlayFabMultiplayer.
+FOUNDATION_EXPORT double PlayFabMultiplayerVersionNumber;
+
+//! Project version string for PlayFabMultiplayer.
+FOUNDATION_EXPORT const unsigned char PlayFabMultiplayerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PFLobby.h>
+#import <PFEntityKey.h>
+#import <PFLobby.h>
+#import <PFMatchmaking.h>
+#import <PFMultiplayer.h>
