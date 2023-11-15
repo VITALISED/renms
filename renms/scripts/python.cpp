143ba1453d319b6e22abb62a99fb81ee51e3c7b2
Will <39478251+VITALISED@users.noreply.github.com>
2023-11-16 09:17:29 +1100

    Add some extra logger calls

--- renms/scripts/python.cpp
+++ renms/scripts/python.cpp
@@ -6,11 +6,32 @@ void __renms_log(const char *lpacMessage)
     Logger->info(lpacMessage);
 }
 
+void __renms_debug(const char *lpacMessage)
+{
+    std::shared_ptr<spdlog::logger> Logger = spdlog::get(GetScriptLoggerName());
+    Logger->debug(lpacMessage);
+}
+
+void __renms_warn(const char *lpacMessage)
+{
+    std::shared_ptr<spdlog::logger> Logger = spdlog::get(GetScriptLoggerName());
+    Logger->warn(lpacMessage);
+}
+
+void __renms_error(const char *lpacMessage)
+{
+    std::shared_ptr<spdlog::logger> Logger = spdlog::get(GetScriptLoggerName());
+    Logger->error(lpacMessage);
+}
+
 PYBIND11_EMBEDDED_MODULE(renms, m)
 {
     m.doc() = "ReNMS Bindings";
 
-    m.def("info", &__renms_log, "Binding for spdlog::info, must be const char*");
+    m.def("info", &__renms_log, "Binding for spdlog::info");
+    m.def("debug", &__renms_debug, "Binding for spdlog::debug");
+    m.def("warn", &__renms_warn, "Binding for spdlog::warn");
+    m.def("error", &__renms_error, "Binding for spdlog::error");
 }
 
 RENMS_BEGIN
@@ -21,7 +42,6 @@ void CreateScriptEnvironment()
 
     try
     {
-        // auto renms_logger = py::module_::import("renms");
         py::exec("import renms\nrenms.info(\"Calling spdlog from Py.\")");
     }
     catch (py::error_already_set const &pythonErr)
--- renms/scripts/python.h
+++ renms/scripts/python.h
@@ -12,6 +12,9 @@ using namespace std::filesystem;
 namespace py = pybind11;
 
 void __renms_log(const char *lpacMessage);
+void __renms_debug(const char *lpacMessage);
+void __renms_warn(const char *lpacMessage);
+void __renms_error(const char *lpacMessage);
 
 RENMS_BEGIN
 
