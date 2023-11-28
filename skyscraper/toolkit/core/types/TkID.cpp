5f633f0a8f0f0367ec2cfbb4854fcdd672e67dad
Will <39478251+VITALISED@users.noreply.github.com>
2023-11-28 16:01:41 +1100

    Add TkID hash calculations

---
+++ skyscraper/toolkit/core/types/TkID.cpp
@@ -0,0 +1,19 @@
+#include <toolkit/core/types/TkID.h>
+
+SKYSCRAPER_BEGIN
+
+template <uint32_t Size>
+uint64_t TkID<Size>::CalcHash()
+{
+    uint64_t hashVal = TK_FNV_OFFSET_BASIS;
+
+    for (int i = 0; i < sizeof(this.mChars); i++)
+    {
+        hash ^= this->mChars[i];
+        hash *= TK_FNV_PRIME;
+    }
+
+    return hashVal;
+}
+
+SKYSCRAPER_END
--- skyscraper/toolkit/core/types/TkID.h
+++ skyscraper/toolkit/core/types/TkID.h
@@ -2,17 +2,17 @@
 
 #include <skyscraper.h>
 
+#include <toolkit/maths/utilities/hash/TkHashUtilities.h>
+
 SKYSCRAPER_BEGIN
 
 template <uint32_t Size>
 union TkID {
     TkID();
 
-    TkID(const char *lpacString)
-    {
-        // uses hashes pretty sure, gonna have to figure out spookyhash later
-        strcpy_s(mChars, Size, lpacString);
-    }
+    TkID(const char *lpacString) { strcpy_s(mChars, Size, lpacString); }
+
+    uint64_t CalcHash();
 
     char mChars[Size / 8];
     uint64_t mUInt64[Size / 64];
