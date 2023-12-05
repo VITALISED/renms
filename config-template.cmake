bc59f068c9378e6658c91e7d5a97817ba316dd15
Will <39478251+VITALISED@users.noreply.github.com>
2023-12-05 22:24:38 +1100

    Update misc licensing

--- config-template.cmake
+++ config-template.cmake
@@ -1,4 +1,4 @@
-# Copyright (C) 2023  VITALISED, tractorbeam
+# Copyright (C) 2023  VITALISED & Contributors
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -16,9 +16,10 @@
 set(RENMS_CONFIG_VERSION 3)
 
 # Sorry for having it in a seperate file instead of having the option stuff be a
-# part of the main CMakeLists.txt, but it's just to make sure these settings don't
-# get synced with the repo.
+# part of the main CMakeLists.txt, but it's just to make sure these settings
+# don't get synced with the repo.
 
 option(SKIP_NMS_CHECK FALSE)
 set(NMS_EXE_PATH "C:/Your/Path/To/NMS/Exe/Dir/Here/NMS.exe")
-set(EXAMPLE_BUILD_TYPE "hook_example") # you can comment this out to not build any examples
+set(EXAMPLE_BUILD_TYPE "hook_example") # you can comment this out to not build
+                                       # any examples
