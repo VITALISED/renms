27b77e5f8c6b77d6504155288981d1a3059ac1b8
Will <39478251+VITALISED@users.noreply.github.com>
2023-12-05 22:31:10 +1100

    Add cross-compile align

--- skyscraper/skyscraper.h
+++ skyscraper/skyscraper.h
@@ -2,19 +2,19 @@
  * @file skyscraper.h
  * @author VITALISED & Contributors
  * @since 2023-12-05
- * 
+ *
  * Copyright (C) 2023  VITALISED & Contributors
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
@@ -47,7 +47,11 @@
 #define SKYSCRAPER_END };
 // clang-format on
 
-#define ALIGN(size) __declspec(align(16))
+#ifdef _MSC_VER
+#define ALIGN(size) __declspec(align(size))
+#else
+#define ALIGN(size) __attribute__((aligned(size)))
+#endif
 
 #define EMPTY_CALL()            return NULL
 #define EMPTY_CALL_VOID()       return
